3a23511bfb2e5406b510eee28b0234fd666e7b27
manalkarrot <manal.karrot@icloud.com>
2021-05-19 00:02:19 +0200

    Ajout des images

--- app/src/main/java/com/example/projetpm/presentation/liste/PokemonAdapter.kt
+++ app/src/main/java/com/example/projetpm/presentation/liste/PokemonAdapter.kt
@@ -3,8 +3,10 @@ package com.example.projetpm.presentation.liste
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.projetpm.R
 
 class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Int) -> Unit)? = null) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
@@ -17,10 +19,12 @@ class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Int) ->
      */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
+        val imageView: ImageView
 
         init {
             // Define click listener for the ViewHolder's View.
             textView = view.findViewById(R.id.pokemon_name)
+            imageView = view.findViewById(R.id.pokemon_img)
 
         }
     }
@@ -49,6 +53,12 @@ class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Int) ->
         viewHolder.itemView.setOnClickListener {
             listener?.invoke(position)
         }
+
+        Glide
+                .with(viewHolder.itemView.context)
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${position + 1}.png")
+                .centerCrop()
+                .into(viewHolder.imageView)
 
     }
 
